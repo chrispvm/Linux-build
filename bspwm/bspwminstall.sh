9b9b721405de790d13b5621ac7bf0fa291a6f389
Chris <c.vanmerwijk@gmail.com>
2021-12-10 20:09:20 +0000

    added bspwm

---
+++ bspwm/bspwminstall.sh
@@ -0,0 +1,11 @@
+sudo apt install bspwm -y
+
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+
+cp ./bspwmrc ~/.config/bspwm/bspwmrc
+cp ./sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+sudo apt install dmenu -y
+
+
--- bspwminstall.sh
+++
@@ -1,7 +0,0 @@
-apt install bspwm -y
-
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
-
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
