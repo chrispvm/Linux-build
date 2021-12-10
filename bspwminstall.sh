fcbe20eeb43c799efb5653635da80b0db9748d41
Chris <c.vanmerwijk@gmail.com>
2021-12-10 14:18:43 +0000

    bspwm stuff

---
+++ bspwminstall.sh
@@ -0,0 +1,7 @@
+apt install bspwm -y
+
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
